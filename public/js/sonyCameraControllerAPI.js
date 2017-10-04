38c63f87ff27a22ee50c1c3c7134a97dc58715bc
Gustavo <gustavo.carvallo@ing.austral.edu.ar>
2017-10-04 13:20:37 -0300

    Adding basic Sony API app

---
+++ public/js/sonyCameraControllerAPI.js
@@ -0,0 +1,143 @@
+var xhr = new XMLHttpRequest();
+var liveviewUrl;
+
+//Liveview encoding useful vars.
+var CRA_LIVEVIEW_MAX_RECEIVE_SIZE = 500000;
+var CRA_LIVEVIEW_COMMON_HEADER_SIZE = 8;
+var CRA_LIVEVIEW_PLAYLOAD_HEADER_SIZE = 128;
+
+function createCORSRequest(method, url) {
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+function getLiveview(){
+  var headerDecode = false;
+  var offset = 0;
+  var self = arguments.callee;
+
+  xhr = createCORSRequest('GET', liveviewUrl);
+  xhr.open('GET', liveviewUrl, true);
+  xhr.overrideMimeType('text\/plain; charset=x-user-defined');
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState == 3) {
+          if(xhr.response.length >= CRA_LIVEVIEW_MAX_RECEIVE_SIZE) {
+              //console.log('finish');
+              xhr.abort();
+              self();
+          }
+
+          if(xhr.response.length >= (CRA_LIVEVIEW_COMMON_HEADER_SIZE + CRA_LIVEVIEW_PLAYLOAD_HEADER_SIZE+offset)) {
+              if(headerDecode == false) {
+                  var startByte = (xhr.responseText.charCodeAt(offset + 0) & 0xff);
+                  var playLoadType = xhr.responseText.charCodeAt(offset + 1) & 0xff;
+                  var sequenceNumber  = (xhr.responseText.charCodeAt(offset + 2) & 0xff) << 8;
+                      sequenceNumber += (xhr.responseText.charCodeAt(offset + 3) & 0xff);
+                  var timeStamp  = (xhr.responseText.charCodeAt(offset + 4) & 0xff) << 24;
+                      timeStamp += (xhr.responseText.charCodeAt(offset + 5) & 0xff) << 16;
+                      timeStamp += (xhr.responseText.charCodeAt(offset + 6) & 0xff) <<  8;
+                      timeStamp += (xhr.responseText.charCodeAt(offset + 7) & 0xff);
+                  var startCode = [(xhr.responseText.charCodeAt(offset + 8) & 0xff), (xhr.responseText.charCodeAt(offset + 9) & 0xff), (xhr.responseText.charCodeAt(offset + 10) & 0xff), (xhr.responseText.charCodeAt(offset + 11) & 0xff)];
+                  var jpegSize  = ((xhr.responseText.charCodeAt(offset + 12) & 0xff) * (256 * 256));
+                      jpegSize += ((xhr.responseText.charCodeAt(offset + 13) & 0xff) * 256);
+                      jpegSize += ((xhr.responseText.charCodeAt(offset + 14) & 0xff));
+                  var paddingSize = xhr.responseText.charCodeAt(offset + 15) & 0xff;
+
+                  // console.log('startByte: ' +  (startByte).toString(16));
+                  //
+                  // console.log('playLoadType: ' +  (playLoadType).toString(16));
+                  // console.log('startCode: ' +  (startCode[0]).toString(16) + (startCode[1]).toString(16) + (startCode[2]).toString(16) + (startCode[3]).toString(16));
+                  //
+                  // console.log('jpegSize: ' +  (jpegSize).toString(16));
+                  // console.log('paddingSize: ' +  (paddingSize).toString(16));
+              }
+
+              if(xhr.response.length >= (CRA_LIVEVIEW_COMMON_HEADER_SIZE + CRA_LIVEVIEW_PLAYLOAD_HEADER_SIZE + jpegSize + offset)) {
+                  binary = '';
+                  for (var i = (CRA_LIVEVIEW_COMMON_HEADER_SIZE + CRA_LIVEVIEW_PLAYLOAD_HEADER_SIZE + offset), len = (CRA_LIVEVIEW_COMMON_HEADER_SIZE + CRA_LIVEVIEW_PLAYLOAD_HEADER_SIZE + offset)+jpegSize; i < len; ++i) {
+                      binary += String.fromCharCode(xhr.responseText.charCodeAt(i) & 0xff);
+                  }
+
+                  var base64 = window.btoa(binary);
+                  if (base64.length > 0 && base64[0] == "/") {
+                      document.getElementById("image").src = "data:image/jpeg;base64," + base64;
+                      offset = CRA_LIVEVIEW_COMMON_HEADER_SIZE + CRA_LIVEVIEW_PLAYLOAD_HEADER_SIZE + offset + jpegSize + paddingSize;
+                      headerDecode = false;
+                      return;
+                  } else {
+                      console.log('What is this?');
+                      xhr.abort();
+                      return;
+                  }
+              }
+              return;
+          }
+      }
+  };
+  xhr.send();
+}
+
+
+
+function abortLiveviewRequest() {
+  xhr.abort();
+}
+
+function startLiveView() {
+  executeMethod("startLiveview", []);
+}
+
+function stopLiveView() {
+  abortLiveviewRequest();
+  executeMethod("stopLiveview", []);
+}
+
+function executeMethod(method, params){
+  var id = 1;
+  var version = "1.0";
+  var endPointUrl = "http://192.168.122.1:10000/sony/camera";
+  var message = JSON.stringify({
+    "method": method,
+    "params": params,
+    "id": 1,
+    "version": "1.0"
+  });
+  console.log(message);
+  console.log(endPointUrl);
+
+  var xhr2 = createCORSRequest('POST', endPointUrl);
+  if (!xhr2) {
+    throw new Error('CORS not supported');
+  }
+
+  xhr2.onreadystatechange = function() {
+    if (xhr2.readyState === 4 && xhr2.status === 200) {
+      console.log(xhr2.responseText);
+      var response = JSON.parse(xhr2.responseText);
+      if (method == "startLiveview") {
+        liveviewUrl = response.result;
+      }
+      console.log(response);
+    }
+  };
+
+  xhr2.send(message);
+}
